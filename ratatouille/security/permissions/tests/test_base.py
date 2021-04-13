82ae83fe14ddc13f7c75afd60a1036869e1525bb
alvarogf97 <ankora45@gmail.com>
2021-04-13 21:32:46 +0200

    finish tests

--- ratatouille/security/permissions/tests/test_base.py
+++ ratatouille/security/permissions/tests/test_base.py
@@ -1,11 +1,36 @@
+from unittest import mock
+from fastapi import HTTPException
+from starlette import status
+
 from ratatouille.common.tests import AsyncRatatouilleTestCase
+from ratatouille.security.permissions import base
 
 
 class TestPermissionRunner(AsyncRatatouilleTestCase):
     """Permission runner tests"""
 
     async def test_permissions_success(self):
-        pass
+        request = ''
+        user = ''
+        mock_permission = mock.Mock(
+            has_permission=mock.AsyncMock(return_value=(True, '')))
+        runner = base.PermissionRunner(mock_permission)
+
+        await runner(request, user)
+        mock_permission.has_permission.assert_called_once_with(request, user)
 
     async def test_permissions_error(self):
-        pass
+        detail = 'Forbidden'
+        request = ''
+        user = ''
+        mock_permission = mock.Mock(
+            has_permission=mock.AsyncMock(side_effect=HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail=detail
+            )))
+        runner = base.PermissionRunner(mock_permission)
+
+        with self.assertRaises(HTTPException):
+            await runner(request, user)
+
+        mock_permission.has_permission.assert_called_once_with(request, user)
--- ratatouille/security/permissions/tests/test_helpers.py
+++ ratatouille/security/permissions/tests/test_helpers.py
@@ -1,8 +1,10 @@
 from ratatouille.common.tests import AsyncRatatouilleTestCase
+from ratatouille.security.permissions import helpers
 
 
 class TestRequireDependency(AsyncRatatouilleTestCase):
     """Require depencency test"""
 
     async def test_wraps_permissions_runner_dependency(self):
-        pass
+        self.assertIsInstance(helpers.require().dependency,
+                              helpers.base.PermissionRunner)
