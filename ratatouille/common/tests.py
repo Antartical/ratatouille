9de07e33c6860c74ea451ad2afd0aaab5594a68b
alvarogf97 <ankora45@gmail.com>
2021-04-13 11:18:39 +0200

    start asyncio tests

--- ratatouille/common/tests.py
+++ ratatouille/common/tests.py
@@ -1,10 +1,40 @@
-from unittest import TestCase
+import json
+import typing
+import unittest
+import pytest
+from functools import partial
 from fastapi.testclient import TestClient
+from pytest_httpx import HTTPXMock
+
 from ratatouille.asgi import app
 
 
-class APITestCase(TestCase):
+def _inject(cls, names):
+    @pytest.fixture(autouse=True)
+    def auto_injector_fixture(self, request):
+        for name in names:
+            setattr(self, name, request.getfixturevalue(name))
+
+    cls._auto_injector_fixture = auto_injector_fixture
+    return cls
+
+
+def auto_inject_fixtures(*names):
+    return partial(_inject, names=names)
+
+
+class APITestCase(unittest.TestCase):
     """TestCase class for API testing."""
 
     def setUp(self):
         self.client = TestClient(app)
+
+
+@auto_inject_fixtures('httpx_mock')
+class AsyncHTTPXTestCase(unittest.IsolatedAsyncioTestCase):
+    """TestCase class for async httpx testing."""
+
+    httpx_mock: HTTPXMock
+
+    def dump(self, payload: typing.Dict) -> bytes:
+        return json.dumps(payload).encode()
--- ratatouille/vendors/tests/test_gandalf.py
+++ ratatouille/vendors/tests/test_gandalf.py
@@ -1,22 +1,42 @@
-import pytest
-from unittest import TestCase
+from ratatouille.common.tests import AsyncHTTPXTestCase
+from ratatouille.vendors import gandalf
 
 
-class TestPingRoute(TestCase):
+class TestGandalfAPI(AsyncHTTPXTestCase):
     """Gandalf vendor test"""
 
-    @pytest.mark.asyncio
     async def test_login_success(self):
-        pass
+        email = 'test@test.com'
+        password = 'test'
+        scopes = ['user:read']
+        access_token = 'test'
+        refresh_token = 'test'
 
-    @pytest.mark.asyncio
+        matched_payload = self.dump({
+            'email': email,
+            'password': password,
+            'scopes': scopes
+        })
+        mocked_response = {
+            'type': 'token',
+            'data': {
+                'access_token': access_token,
+                'refresh_token': refresh_token,
+            }
+        }
+
+        self.httpx_mock.add_response(
+            json=mocked_response, match_content=matched_payload)
+        tokens = await gandalf.login(email, password, scopes)
+
+        assert tokens.access_token == access_token
+        assert tokens.refresh_token == refresh_token
+
     async def test_login_error(self):
         pass
 
-    @pytest.mark.asyncio
-    async def me_success(self):
+    async def test_me_success(self):
         pass
 
-    @pytest.mark.asyncio
-    async def me_error(self):
+    async def test_me_error(self):
         pass
