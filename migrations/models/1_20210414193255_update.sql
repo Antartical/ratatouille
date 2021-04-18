6b216dd7cc4dc6af1c1a068d9362c11178f153e7
alvarogf97 <ankora45@gmail.com>
2021-04-18 20:51:29 +0200

    unify migrations

--- migrations/models/1_20210414193255_update.sql
+++
@@ -1,6 +0,0 @@
--- upgrade --
-ALTER TABLE "user" ADD "_index_id" VARCHAR(255);
-CREATE UNIQUE INDEX "uid_user_uuid_863a0b" ON "user" ("uuid");
--- downgrade --
-ALTER TABLE "user" DROP COLUMN "_index_id";
-DROP INDEX "idx_user_uuid_863a0b";
--- migrations/models/2_20210415093610_update.sql
+++
@@ -1,4 +0,0 @@
--- upgrade --
-CREATE UNIQUE INDEX "uid_user__index__a410b4" ON "user" ("_index_id");
--- downgrade --
-DROP INDEX "idx_user__index__a410b4";
