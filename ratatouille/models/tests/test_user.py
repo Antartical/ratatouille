b5535b6c8e9148e874dcde7439c9c74df0448805
alvarogf97 <ankora45@gmail.com>
2021-06-16 16:17:00 +0200

    test user tyo document

--- ratatouille/models/tests/test_user.py
+++ ratatouille/models/tests/test_user.py
@@ -1,5 +1,16 @@
 from ratatouille.common import test
+from ratatouille.models.factories import user_factories
 
 
 class UserModelTest(test.AsyncRatatouilleTestCase):
-    pass
+
+    @test.db_test
+    async def test_to_document(self):
+        user = await user_factories.user_factory()
+        expected_response = {
+            'id': user.id,
+            'uuid': user.uuid,
+            'email': user.email,
+            'name': user.name
+        }
+        self.assertEqual(expected_response, user.to_document)
